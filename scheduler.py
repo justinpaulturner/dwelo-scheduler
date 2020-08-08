9d608d0daf74b2931b851f533e967642e85fb2f6
Justin Turner <justin@justinpturner.com>
2020-08-08 10:26:19 -0500

    improved verbose

--- scheduler.py
+++ scheduler.py
@@ -5,8 +5,11 @@ import time
 ##################################################################################
 
 d = Dwelo()
-d.launch_chrome(headless = True)
+print("Logging in to Dwello.")
+d.launch_chrome(headless = False)
 d.load_cookies()
+print("Logged in.")
+print("Sleeping until next action")
 
 schedule.every().day.at("19:30").do(d.set_cool, 66) # Sets AC to 66 degrees at 7:30 PM
 schedule.every().day.at("06:30").do(d.set_cool, 73) # Sets AC to 73 degrees at 6:30 AM
@@ -14,12 +17,15 @@ schedule.every().day.at("06:30").do(d.set_cool, 73) # Sets AC to 73 degrees at 6
 while True:
     try:
         time.sleep(1)
+        print(f"{d.current_time()} |  Sleeping", end = '')
         schedule.run_pending()
         time.sleep(1)
     except:
+        print("Failed. Trying to log in again.")
+        time.sleep(5)
         d.quit()
         time.sleep(1)
         d.launch_chrome()
         time.sleep(1)
         d.load_cookies()
-        time.sleep()
+        time.sleep(3600)
